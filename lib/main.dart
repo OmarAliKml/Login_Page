a5d0b80d5f703a67cf097bb7f84af98d20f10e2c
Omar Ali <omar.ali.kml@gmail.com>
2024-06-24 16:16:05 +0200

    Change color on login screen

--- lib/main.dart
+++ lib/main.dart
@@ -32,9 +32,9 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.teal,
         title: const Text(
           'Login',
           style: TextStyle(
